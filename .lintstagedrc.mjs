2171c6f190ce6ccdc575f5190444b2c4345c940f
M.S.T.O.P <mstop4@gmail.com>
2024-04-18 21:36:38 -0400

    prettier changes

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,17 +1,17 @@
 export default {
   '**/*.ts': [
-    `npx prettier *.ts --write`,
+    `npx prettier **/*.ts --write`,
     'npx tsc --noEmit',
-    `npx eslint *.ts`,
+    `npx eslint **/*.ts`,
   ],
   '**/*.js': [
-    `npx prettier *.js --write`,
-    `npx eslint *.js`,
+    `npx prettier **/*.js --write`,
+    `npx eslint **/*.js`,
   ],
   '**/*.json': [
-    `npx prettier *.json --write`,
+    `npx prettier **/*.json --write`,
   ],
   '**/*.css': [
-    `npx prettier *.css --write`,
+    `npx prettier **/*.css --write`,
   ],
 };
